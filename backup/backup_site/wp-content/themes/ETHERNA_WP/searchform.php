61d959021f46be54a605c5f3576a46759b442821
gospel1234 <gospel1234@p3nlhftpg101.shr.prod.phx3.secureserver.net>
2016-05-17 13:32:10 -0700

    Removed backup folder

--- backup/backup_site/wp-content/themes/ETHERNA_WP/searchform.php
+++
@@ -1,6 +0,0 @@
-<div class="search">
-	<form id="search_form" action="<?php bloginfo('url'); ?>" method="get">
-		<input id="search_input" type="text" value="<?php if(trim(wp_specialchars($s,1))!='') echo trim(wp_specialchars($s,1));else echo ' ';?>" name="s">
-		<input id="search_submit" type="submit" value="<?php _e('search'); ?>" name="SearchSubmit">
-	</form>
-</div>
